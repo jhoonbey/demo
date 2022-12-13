337b848049ce915476ef0a8f3db019cef9053cd7
Ceyhun A. Rahimov <RahimovCA@kapitalho.az>
2022-12-13 18:43:18 +0400

    demo files added

---
+++ Account.cs
@@ -0,0 +1,14 @@
+﻿namespace demo;
+
+public class Account : BaseEntity
+{
+    public string AccountNumber { get; set; }
+
+    public decimal Balance { get; set; }
+
+    public int UserId { get; set; }
+
+    public virtual ICollection<Transaction> FromTransactions { get; set; }
+
+    public virtual ICollection<Transaction> ToTransactions { get; set; }
+}
---
+++ Transaction.cs
@@ -0,0 +1,16 @@
+﻿namespace demo;
+
+public class Transaction : BaseEntity
+{
+    public string TransactionNumber { get; set; }
+
+    public int FromAccountId { get; set; }
+
+    public int ToAccountId { get; set; }
+
+    public decimal Amount { get; set; }
+
+    public virtual Account FromAccount { get; set; }
+
+    public virtual Account ToAccount { get; set; }
+}
